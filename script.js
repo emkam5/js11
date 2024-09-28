3c60cc8df86abb818bbac686a2516136774ee684
emkam5 <wertysedt@gmail.com>
2024-09-28 10:37:47 +0300

    js11

---
+++ script.js
@@ -0,0 +1,12 @@
+let drop = document.getElementById('dropdown-items');
+dropdown.addEventListener('mouseover', function () {
+    drop.style.display = 'flex';
+    drop.style.transform = 'translateY(0)';
+    drop.style.transition = 'transform 0.3s ease-in-out';
+});
+dropdown.addEventListener('mouseout', function () {
+    drop.style.transform = 'translateY(-200px)';
+    drop.style.transition = 'transform 0.3s ease-in-out'; 
+        drop.style.display = 'none';
+
+});
